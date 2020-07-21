0408b511e47be40ed3bf314c6de97f44b179fd41
Yves Shema <admin@rysapp.com>
2020-07-21 13:32:46 -0700

    Updates

---
+++ precache-manifest.fa67a5dc9d7dbc7d67f84f0b157e14a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f05c03dc5a1ab916ffc1d189e3299c64",
+    "url": "/task-tracker/index.html"
+  },
+  {
+    "revision": "61fba79b897f2a2a5962",
+    "url": "/task-tracker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bce941aa8763d108717e",
+    "url": "/task-tracker/static/js/2.ddd70e4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/task-tracker/static/js/2.ddd70e4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61fba79b897f2a2a5962",
+    "url": "/task-tracker/static/js/main.7f59a5f8.chunk.js"
+  },
+  {
+    "revision": "ddb853cd9f12092cbccf",
+    "url": "/task-tracker/static/js/runtime-main.b5f8217e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/task-tracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
